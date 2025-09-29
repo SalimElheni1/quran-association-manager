f6327f2011d0658eaed9c74c4a85c6e136cd16c5
SalimElheni1 <elheni.selim@gmail.com>
2025-09-29 22:04:15 +0100

    Fix UI

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from '@renderer/App';
 import { AuthProvider } from '@renderer/contexts/AuthContext';
-import 'bootstrap/dist/css/bootstrap.rtl.min.css'; // Import Bootstrap RTL CSS
+import '@renderer/styles/custom-bootstrap.scss'; // Import custom Bootstrap build
 import '@renderer/styles/index.css';
 import { ToastContainer } from 'react-toastify';
 
